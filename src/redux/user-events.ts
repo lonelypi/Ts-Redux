a55d329ce38ff8b219edfd41765d87e14830e394
Pedro <pedroivodefaria@gmail.com>
2020-08-21 00:21:53 -0300

    Start project

---
+++ src/redux/user-events.ts
@@ -0,0 +1,10 @@
+import { act } from "@testing-library/react"
+
+const userEventsReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+export default userEventsReducer
